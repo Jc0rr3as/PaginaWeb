b06cfc6939475d8194de620acca5f1fc1dc86c19
Jc0rr3as <jc0rr3as@gmail.com>
2026-09-09 20:47:28 -0500

    metodo registrar terminado

---
+++ metodos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class metodos {
+    Stack<objPagina> paginas = new Stack<>();
+    Scanner sc = new Scanner(System.in);
+    public Stack<objPagina> registro(){
+        boolean continuar = true;
+        while (continuar) {
+            System.out.print("Ingrese la URL de la página: ");
+            String url = sc.nextLine();
+            System.out.print("Ingrese el título de la página: ");
+            String titulo = sc.nextLine();
+            System.out.print("Ingrese la fecha de acceso (formato: dd/mm/aaaa): ");
+            String fechaAcceso = sc.nextLine();
+
+            objPagina pagina = new objPagina(url, titulo, fechaAcceso);
+            paginas.push(pagina);
+
+            System.out.print("¿Desea agregar otra página? (s/n): ");
+            String respuesta = sc.nextLine();
+            if (respuesta.equalsIgnoreCase("n")) {
+                continuar = false;
+            }
+        }
+        return paginas;
+    }
+}
